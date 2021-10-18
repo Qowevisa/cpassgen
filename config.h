9ce9f51b0fdfc88adb24743bd72302b323c6d673
Qowevisa <qowevisa@gmail.com>
2021-10-18 18:10:20 +0300

    Some state

--- config.h
+++ config.h
@@ -1,9 +1,17 @@
+#ifndef __cpassgen_h
+#define __cpassgen_h
+
+#define bs 16
+#define sq 64
+#define mb 128
 
 /* Please edit this seed for your needs and don't forget seed or binary file to keep hash-function's work be the same*/
-char code[16] = {
+char code[bs] = {
 	0, 0, 0, 0,
 	0, 0, 0, 0,
 	0, 0, 0, 0,
 	0, 0, 0, 0
 };
 
+
+#endif // __cpassgen_h
