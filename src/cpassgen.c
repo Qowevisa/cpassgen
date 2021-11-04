39e009d40c3a5aec198a26a42d18534f15617f2b
Qowevisa <qowevisa@gmail.com>
2021-11-04 13:45:05 +0200

    Add prime_to_bits()

--- src/cpassgen.c
+++ src/cpassgen.c
@@ -6,6 +6,29 @@
 #include "../inc/math.h"
 #include "../inc/gnr.h"
 
+// out will be char[4];
+void prime_to_bits(unsigned int prime, unsigned char **out) {
+	(*out) = (unsigned char*)malloc(4);
+	unsigned char tmp_byte;
+	unsigned char bit;
+	float prm_f = Q_rsqrt(prime);
+	for (int i = 0; i < 4; i++) {
+        tmp_byte = 0;
+        bit = 128;
+        while (bit > 0) {
+            prm_f = prm_f * 2;
+            unsigned int rest = (unsigned int)prm_f;
+            prm_f -= rest;
+            if (rest == 1) {
+                tmp_byte += bit;
+                bit /= 2;
+            } else {
+                bit /= 2;
+            }
+        }
+        (*out)[i] = tmp_byte;
+	}
+}
 
 int main(int argc, char *argv[]) {
 	char first_block[sq];
